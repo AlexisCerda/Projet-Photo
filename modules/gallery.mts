3699107f1455b7bf52e65a4687e7315a15ae5826
Alexis CERDA <alexis.cerda-de-almeida-vilaca2@etu.univ-lorraine.fr>
2026-05-28 23:27:27 +0200

    pas besoin de variable dans gallery car on fait passer en parametre URI qui faut donc on met a jour la gallery avec le retour de la fonction

--- modules/gallery.mts
+++ modules/gallery.mts
@@ -2,10 +2,6 @@ import { API, API_LOW, reponsePhotos } from "./config.mts";
 import { loadResource } from "./photoloeader.mts";
 
 let currentGallery: reponsePhotos | null = null;
-let nextGallery: reponsePhotos | null = null;
-let prevGallery: reponsePhotos | null = null;
-let firstGallery: reponsePhotos | null = null;
-let lastGallery: reponsePhotos | null = null;
 
 function buildUrl(href?: string): string {
     if (!href) return `${API}/photos`;
@@ -14,41 +10,13 @@ function buildUrl(href?: string): string {
 }
 
 export async function load(uri?: string): Promise<reponsePhotos> {
-  const target = uri ?? `${API}/photos`;
+  const target = buildUrl(uri);
   const galerie = await loadResource<reponsePhotos>(target);
   currentGallery = galerie;
-
-  const targetNext = buildUrl(currentGallery?.links?.next?.href);
-  nextGallery = await loadResource<reponsePhotos>(targetNext);
-
-  const targetPrev = buildUrl(currentGallery?.links?.prev?.href);
-  prevGallery = await loadResource<reponsePhotos>(targetPrev);
-
-  const targetFirst = buildUrl(currentGallery?.links?.first?.href);
-  firstGallery = await loadResource<reponsePhotos>(targetFirst);
-  
-  const targetLast = buildUrl(currentGallery?.links?.last?.href);
-  lastGallery = await loadResource<reponsePhotos>(targetLast);
 
   return galerie;
 }
 
 export function getCurrentGallery(): reponsePhotos | null {
   return currentGallery;
-}
-
-export function getNextGallery(): reponsePhotos | null {
-  return nextGallery;
-}
-
-export function getPreviousGallery(): reponsePhotos | null {
-  return prevGallery;
-}
-
-export function getFirstGallery(): reponsePhotos | null {
-  return firstGallery;
-}
-
-export function getLastGallery(): reponsePhotos | null {
-  return lastGallery;
 }
--- ts/index.ts
+++ ts/index.ts
@@ -1,13 +1,18 @@
-
-import { load } from "../modules/gallery.mts";
+import { getCurrentGallery, load } from "../modules/gallery.mts";
 import { display_galerie } from "../modules/gallery_ui.mts";
 
 const loadButton = document.getElementById("load-gallery");
-
+const nextButton= document.getElementById("Nload-gallery");
+const previousButton= document.getElementById("Pload-gallery");
+const firstButton= document.getElementById("Fload-gallery");
+const lastButton= document.getElementById("Lload-gallery");
 if (loadButton) {
   loadButton.addEventListener("click", async () => {
     try {
-      const galerie = await load();
+      var galerie = getCurrentGallery();
+      if (!galerie) {
+        galerie = await load();
+      }
       display_galerie(galerie);
     } catch (error) {
       console.error("Impossible de charger la galerie.", error);
@@ -16,3 +21,75 @@ if (loadButton) {
 } else {
   console.warn("Bouton de chargement introuvable.");
 }
+if (nextButton) {
+  nextButton.addEventListener("click", async () => {
+    try {
+      const current = getCurrentGallery();
+      if (current && current.links?.next?.href) {
+        const newGalerie = await load(current.links.next.href);
+        display_galerie(newGalerie);
+      } else {
+        console.warn("Il n'y a pas de page suivante ou la galerie n'a pas encore été chargée initialement.");
+      }
+    } catch (error) {
+      console.error("Impossible de charger la galerie suivante.", error);
+    }
+  });
+} else {
+  console.warn("Bouton de chargement suivant introuvable.");
+}
+
+if (previousButton) {
+  previousButton.addEventListener("click", async () => {
+    try {
+      const current = getCurrentGallery();
+      if (current && current.links?.prev?.href) {
+        const newGalerie = await load(current.links.prev.href);
+        display_galerie(newGalerie);
+      } else {
+        console.warn("Il n'y a pas de page précédente ou la galerie n'a pas encore été chargée initialement.");
+      }
+    } catch (error) {
+      console.error("Impossible de charger la galerie précédente.", error);
+    }
+  });
+} else {
+  console.warn("Bouton de chargement précédent introuvable.");
+}
+
+if (firstButton) {
+  firstButton.addEventListener("click", async () => {
+    try {
+      const current = getCurrentGallery();
+      if (current && current.links?.first?.href) {
+        const newGalerie = await load(current.links.first.href);
+        display_galerie(newGalerie);
+      } else {
+        console.warn("Il n'y a pas de première page ou la galerie n'a pas encore été chargée initialement.");
+      }
+    } catch (error) {
+      console.error("Impossible de charger la première page.", error);
+    }
+  });
+} else {
+  console.warn("Bouton de chargement première page introuvable.");
+}
+
+if (lastButton) {
+  lastButton.addEventListener("click", async () => {
+    try {
+      const current = getCurrentGallery();
+      if (current && current.links?.last?.href) {
+        const newGalerie = await load(current.links.last.href);
+        display_galerie(newGalerie);
+      } else {
+        console.warn("Il n'y a pas de dernière page ou la galerie n'a pas encore été chargée initialement.");
+      }
+    } catch (error) {
+      console.error("Impossible de charger la dernière page.", error);
+    }
+  });
+} else {
+  console.warn("Bouton de chargement dernière page introuvable.");
+}
+
