bfefe9b85ec0551f74eb6cff9639d66a79a96391
Logan Saget <saget.logan@gmail.com>
2026-05-28 15:14:00 +0200

    question 1

--- index.ts
+++ index.ts
@@ -4,19 +4,29 @@ import { loadPicture, Photo } from "./photoloeader.mts";
  let mainSection = document.getElementById("la_photo");
 
 async function afficherPhoto(idPicture : number){
-    const photo : Photo = loadPicture(2);
+    const photo = await loadPicture(idPicture);
+    
+    if (!photo) {
+        console.error(`Photo ${idPicture} introuvable.`);
+        return;
+    }
 
     const template1 = document.getElementById('template1')?.innerHTML;
     const template1Final = Handlebars.compile(template1);
 
     if (mainSection) {
-                mainSection.innerHTML = template1FInal({
-                    name: books[i].name,
-                    isbn: books[i].isbn,
-                    authors: books[i].authors.join(", "),
-                    numberOfPages: books[i].numberOfPages,
-                    characters: books[i].characters.length,
-                    topCharacters: books[i].characters.slice(0, 10)
+                mainSection.innerHTML = template1Final({
+                    id: photo.id,
+                    url: photo.url,
+                    width : photo.width,
+                    height: photo.height,
+                    titre: photo.titre,
+                    descr: photo.descr,
+                    format: photo.format,
+                    size : photo.size,
+                    categorie: photo.categorie,
+                    comments : photo.comments,
+                    links : photo.links,
                 });
             }
    
--- photoloeader.mts
+++ photoloeader.mts
@@ -1,13 +1,18 @@
 const API = "https://webetu.iutnc.univ-lorraine.fr/www/canals5/phox/api";
 
- export interface Photo {
-        id: string,
-        titre: string,
-        file: string,
-        thumbnail: string,
-        original: string,
-        links: string,
-    }
+export interface Photo {
+    id: number,
+    titre: string,
+    descr: string,
+    format: string,
+    size: number,
+    width: number,
+    height: number,
+    url: string,
+    links: string,
+    categorie: string,
+    comments: string,
+}
 
 
 export async function loadPicture(idPicture: number) {
